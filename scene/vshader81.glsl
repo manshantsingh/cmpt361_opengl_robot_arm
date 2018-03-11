68409c1708e15805a7e07bc7dde0109eb9bae418
Manshant Singh Kohli <me@manshantsingh.com>
2018-03-11 14:03:04 -0700

    cloned robot to scene (yet to change)

---
+++ scene/vshader81.glsl
@@ -0,0 +1,16 @@
+#version 150 
+
+in  vec4 vPosition;
+in  vec4 vColor;
+out vec4 color;
+
+// uniform vec4 ChosenColor;
+uniform mat4 ModelView;
+uniform mat4 Projection;
+
+void main() 
+{
+    color = vColor;
+    // color = ChosenColor;
+    gl_Position = Projection * ModelView * vPosition;
+} 
